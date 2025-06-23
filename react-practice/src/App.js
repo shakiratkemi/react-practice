c22be99237cfaac224b4e21d67a17464b115c865
shakirat <shakiratganiyy@gmail.com>
2025-06-23 16:32:18 +0100

    Updated the app

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
+      <Greet name="Farhan" surName="Adeyemi">
+        <p>I am five years old</p>
+        <p>I am in Lotus Class</p>
+      </Greet>
+      <Greet name="Faaiz" surName="Adeyemi">
+        <p>I am four years old</p>
+        <p>I am in Primrose Class</p>
+      </Greet>
+      <Welcome name="Farhan" surName="Adeyemi" />
+      <Welcome name="Faaiz" surName="Adeyemi" />
+      <Hello />
+      <Message />
     </div>
   );
 }
